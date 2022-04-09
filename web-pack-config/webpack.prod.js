53c6772b92cc9f44edcda6574a2d00ed7e575bad
Amarjit <jha.amarjit@gmail.com>
2022-04-09 17:09:17 +0530

    Checking with docs

--- web-pack-config/webpack.prod.js
+++ web-pack-config/webpack.prod.js
@@ -6,6 +6,6 @@ const common = require("./webpack.common");
 module.exports = merge(common, {
   mode: "production",
   output: {
-    path: path.resolve(__dirname, "../root"),
+    path: path.resolve(__dirname, "../docs"),
   },
 });
