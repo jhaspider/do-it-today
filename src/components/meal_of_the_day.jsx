d1f12f42ab56d5f601ff0322aff84767be6ec97b
Amarjit <jha.amarjit@gmail.com>
2022-04-11 11:15:17 +0530

    New Version : 17

--- src/components/meal_of_the_day.jsx
+++ src/components/meal_of_the_day.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
-import { getCurDay } from "../utils/utils";
+import React from "react";
 const MealOfTheDay = ({ next_meal, action }) => {
   return (
-    <div className={`mt-5 border-2 rounded-lg p-6 ${next_meal && next_meal.time === action.time ? "border-indigo-600 bg-indigo-100" : "border-gray-300"}`}>
-      <h2>{action.time}</h2>
-      {action.items.map((item, ind) => {
-        return <p key={`${action.time}-action-${ind}`}>{item}</p>;
-      })}
+    <div className="relative">
+      {next_meal && next_meal.time === action.time && <p className="absolute bg-indigo-600 w-fit px-4 top-[-10] left-5 rounded-md text-white">NEXT</p>}
+      <div className={`mt-5 border-2 rounded-lg p-6 ${next_meal && next_meal.time === action.time ? "border-indigo-600 bg-indigo-100" : "border-gray-300"}`}>
+        <h2>{action.time}</h2>
+        {action.items.map((item, ind) => {
+          return <p key={`${action.time}-action-${ind}`}>{item}</p>;
+        })}
+      </div>
     </div>
   );
 };
