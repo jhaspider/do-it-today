39f56a57e6fa71f062ed6d82f6b0e858444ca6d6
Amarjit <jha.amarjit@gmail.com>
2022-06-12 10:46:06 +0530

    Menu optimised after second visit to doctor

--- src/components/pick_day.jsx
+++ src/components/pick_day.jsx
@@ -3,6 +3,7 @@ import { days, getCurDay } from "../utils/utils";
 
 const PickDay = ({ onSel }) => {
   const curDay = getCurDay();
+
   return (
     <div className=" bg-indigo-600 w-full h-full fixed top-0 left-0 flex flex-col items-center">
       <img src="icons/close.png" className="w-12 mt-10 cursor-pointer" onClick={() => onSel()} />
